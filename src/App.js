679677fe832431f4bab986da90cef5cd3e41e871
Parth Makadiya <makadiyaparth98@gmail.com>
2023-06-02 20:40:07 +0530

    typing test game with 1 word

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    fetchWord();
+  }, []);
+
+  const fetchWord = async () => {
+    try {
+      const response = await axios.get(
+        "https://random-word-api.herokuapp.com/word?number=10"
+      );
+      setWord(response.data[0]);
+    } catch (error) {
+      console.error("Error fetching word:", error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+    checkWord();
+  };
+  
+  
+  const getInputStyle = () => {
+    if (result === 'Incorrect!') {
+      return { borderColor: 'red' };
+    }
+    return { borderColor: 'green' };
+  };
+
+  const checkWord = () => {
+    if (input === word) {
+      setResult("Correct!");
+      fetchWord();
+      setInput("");
+    } else if (word.startsWith(input)) {
+      setResult("");
+    } else {
+      setResult("Incorrect!");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Typing Test Game</h1>
+      <p>{word}</p>
+      <input type="text" value={input} onChange={handleInputChange} style={getInputStyle()} autoFocus />
     </div>
   );
 }
